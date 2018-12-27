c0776a7297e6127a26636efa4e7d253500aef8c6
Kivanc Enes Beser <beserkivanc@gmail.com>
2018-12-27 14:04:02 +0300

    first commit

---
+++ Udacity Quiz-JuliaJames(4-1).js
@@ -0,0 +1,21 @@
+/*
+ * Programming Quiz: JuliaJames (4-1)
+ */
+
+var x = 1;
+
+while (x <= 20 /* your stop condition goes here */) {
+  if (x % 3 === 0 && x % 5 === 0) {
+    console.log("JuliaJames");
+  } else if (x % 5 === 0) {
+    console.log("James");
+  } else if (x % 3 === 0) {
+    console.log("Julia");
+  } else {
+    console.log(x);
+  }
+  x++;
+  // check divisibility
+  // print Julia, James, or JuliaJames
+  // increment x
+}
