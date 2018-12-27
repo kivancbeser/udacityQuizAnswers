b2b4c1603c92887777631b258423d3e8a94e03fe
Kivanc Enes Beser <beserkivanc@gmail.com>
2018-12-27 14:30:03 +0300

    Added new answers

---
+++ Udacity Quiz-Factorial! (4-7).js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+solution = 12;
+for (var factorial = 1; factorial < 12; factorial++) {
+  solution *= factorial;
+}
+console.log(solution);
